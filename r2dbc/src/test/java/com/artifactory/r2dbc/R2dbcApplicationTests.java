6eeb86d9dd97d7ae622395877bbfecac603fdbf3
fmontano <fmontano@bancolombia.com.co>
2024-10-08 09:54:15 -0500

    Ejercicio R2DBC

---
+++ r2dbc/src/test/java/com/artifactory/r2dbc/R2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artifactory.r2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2dbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
