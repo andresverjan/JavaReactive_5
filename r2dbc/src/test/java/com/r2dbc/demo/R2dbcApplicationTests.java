726d41c1dcdeda4f496d446647f762739e6475e6
Lausnay Rodriguez <lausrodr@bancolombia.com.co>
2024-10-09 17:37:19 -0500

    Conexión a DB con r2dbc

---
+++ r2dbc/src/test/java/com/r2dbc/demo/R2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r2dbc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2dbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
