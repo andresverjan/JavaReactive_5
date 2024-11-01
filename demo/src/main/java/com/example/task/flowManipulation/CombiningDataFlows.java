1b656b5e94f071919218cb10c2c0f072e97ec633
Luisa vargas gonzalez <luisavg9410@gmail.com>
2024-10-31 20:42:05 -0500

    Combination of Data Flows

---
+++ demo/src/main/java/com/example/task/flowManipulation/CombiningDataFlows.java
@@ -0,0 +1,95 @@
+package com.example.task.flowManipulation;
+
+import reactor.core.CoreSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.task.flowManipulation.Workshop.getPeople;
+
+public class CombiningDataFlows {
+
+    public List<Person> createPeopleList1() {
+         return getPeople().subList(0,6);
+    }
+
+    public List<Person> createPeopleList2() {
+        return getPeople().subList(6,12);
+
+    }
+
+    public Flux<Person> createFluxPerson1() {
+        return Flux.fromIterable(createPeopleList1());
+    }
+
+    public Flux<Person> createFluxPerson2() {
+        return Flux.fromIterable(createPeopleList2());
+    }
+
+    private Flux<String> createDocumentNumberPersonFlux(){
+        List<String> documentNumberPersonList = new ArrayList<>(
+                List.of("11111111", "9999999", "3333333333", "88888888","66666666"));
+        return Flux.fromIterable(documentNumberPersonList);
+    }
+
+    private Flux<String> createStatusFlux(){
+        List<String> documentNumberPersonList = new ArrayList<>(
+                List.of("INACTIVO", "ACTIVO", "BLOQUEADO", "TERMINADO"));
+        return Flux.fromIterable(documentNumberPersonList);
+    }
+
+    static Flux<String> createFluxEmpty() {
+        return Flux.empty();
+    }
+
+    //1: Uso básico de merge
+    //Combina dos flujos en uno solo sin importar el orden y muestra el nombre del cliente junto con su ciudad.
+    public void showName(){
+        Flux<Person> fluxMerged = Flux.merge(createFluxPerson1(),createFluxPerson2());
+        fluxMerged.subscribe(p -> System.out.println("Nombre: " + p.getName()));
+    }
+
+    //2: Uso básico de zip
+    //Combina dos flujos emparejando sus elementos y muestra la suma del saldo total.
+    public void join2Flux(){
+        Flux<Tuple2<Person, String>> combinedZip = Flux.zip(createFluxPerson1(), createDocumentNumberPersonFlux());
+
+        combinedZip
+                .doOnNext(p -> System.out.println("Nombre: " + p.getT1().getName() + " Documento: " + p.getT2() + " Saldo: " +p.getT1().getBalance()))
+                .map(p-> p.getT1().getBalance())
+                .reduce(0.0,Double::sum)
+                .subscribe(sum -> System.out.println( "Suma del saldo total: " + sum));
+    }
+
+    //3: Uso avanzado de combineLatest
+    // Combina dos flujos tomando el último valor emitido por cada uno y
+    // muestra la ciudad junto con su respectivo estado.
+    public void joinWithStatus(){
+        Flux<String> combinedFlow = Flux.combineLatest(createFluxPerson1().map(Person::getCity),
+                createStatusFlux().map(s -> s),
+               (city, status) -> city + " " + status);
+
+        combinedFlow.subscribe(System.out::println);
+    }
+
+    // 4: Uso básico de concat
+    //Concatenar dos flujos en uno solo manteniendo el orden e imprimiendo todos los nombres.
+    public void concatFlux(){
+        Flux<String> concatPersonFlux = Flux.concat(createFluxPerson1().map(Person::getName),
+                createFluxPerson2().map(Person::getName));
+        concatPersonFlux.subscribe(System.out::println);
+    }
+
+    //5: Uso avanzado de switchIfEmpty
+    //Proporcionar un flujo alternativo si el original está vacío e imprime un mensaje personalizado.
+    public void alternativeFlux(){
+        createFluxEmpty()
+                .map(s->s)
+                .switchIfEmpty(Flux.just("Flux Original vacio"))
+        .subscribe(System.out::println);
+    }
+
+}
--- demo/src/main/java/com/example/task/flowManipulation/Workshop.java
+++ demo/src/main/java/com/example/task/flowManipulation/Workshop.java
@@ -11,7 +11,6 @@ import java.util.List;
 public class Workshop {
     public static void main(String[] args) {
 
-
         System.out.println("*************** 1 - Manipulación de Flujos de Datos ***************");
         ManipulationFlow manipulationFlow = new ManipulationFlow();
 
@@ -30,7 +29,7 @@ public class Workshop {
         System.out.println("\n--------------- 5 - Uso avanzado de flatMap ---------------");
         manipulationFlow.statusAccountByAddress();
 
-        System.out.println("\n\n*************** 2 - Gestión de Errores en Flujos ***************");
+        System.out.println("*************** 2 - Gestión de Errores en Flujos ***************");
         FlowErrorManagement flowErrorManagement = new FlowErrorManagement();
 
         System.out.println("\n--------------- 1 - Uso básico de onErrorReturn ---------------");
@@ -48,14 +47,24 @@ public class Workshop {
         System.out.println("\n--------------- 5 - Ignorar errores con onErrorContinue ---------------");
         flowErrorManagement.ignoreError();
 
+        System.out.println("*************** 3 - Combinación de Flujos de Datos ***************");
+        CombiningDataFlows combiningDataFlows = new CombiningDataFlows();
 
+        System.out.println("\n--------------- 1 - Uso básico de merge ---------------");
+        combiningDataFlows.showName();
 
+        System.out.println("\n--------------- 2 - Uso básico de zip ---------------");
+        combiningDataFlows.join2Flux();
 
+        System.out.println("\n--------------- 3 - Uso avanzado de combineLatest ---------------");
+        combiningDataFlows.joinWithStatus();
 
+        System.out.println("\n--------------- 4 - Uso avanzado de combineLatest ---------------");
+        combiningDataFlows.concatFlux();
 
-
-
-    }
+        System.out.println("\n--------------- 5 - Uso avanzado de switchIfEmpty ---------------");
+        combiningDataFlows.alternativeFlux();
+ }
 
     static List<Person> getPeople() {
         String[] addresses1 = new String[]{"Avenida 15", "Calle 10"};
@@ -64,18 +73,18 @@ public class Workshop {
 
         List<Person> people =  new ArrayList<>();
 
-        people.add(new Person("Luisa", "Manchester", addresses1,9785));
-        people.add(new Person("Andres", "Medellin", addresses2,150846));
-        people.add(new Person("Cristian", "Bogota", addresses3,789416));
-        people.add(new Person("Juan", "Cali", addresses1,5984843));
-        people.add(new Person("Maria", "Madrid", addresses2,-154));
-        people.add(new Person("Ana", "Lima", addresses3,7894));
-        people.add(new Person("Pedro", "Londres", addresses3,4873));
-        people.add(new Person("Pablo", "Bogota", addresses3,89416));
-        people.add(new Person("Cindy", "Buga", addresses1,3984842));
-        people.add(new Person("Fernando", "Madrid", addresses2,154));
-        people.add(new Person("Laura", "Lima", addresses3,9894));
-        people.add(new Person("Julia", "Barcelona", addresses3,4873));
+        people.add(new Person("1 Luisa", "Manchester", addresses1,9785));
+        people.add(new Person("2 Andres", "Medellin", addresses2,150846));
+        people.add(new Person("3 Cristian", "Bogota", addresses3,789416));
+        people.add(new Person("4 Juan", "Cali", addresses1,5984843));
+        people.add(new Person("5 Maria", "Madrid", addresses2,-154));
+        people.add(new Person("6 Ana", "Lima", addresses3,7894));
+        people.add(new Person("7 Pedro", "Londres", addresses3,4873));
+        people.add(new Person("8 Pablo", "Bogota", addresses3,89416));
+        people.add(new Person("9 Cindy", "Buga", addresses1,3984842));
+        people.add(new Person("10 Fernando", "Madrid", addresses2,154));
+        people.add(new Person("11 Laura", "Lima", addresses3,9894));
+        people.add(new Person("12 Julia", "Barcelona", addresses3,4873));
 
         return people;
     }
